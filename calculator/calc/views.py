553f046e9304c8964483de5c71f51c61d59411c3
abbas <bmnswry27@gmail.com>
2024-11-12 22:08:02 +0330

    start project django and code views.py

---
+++ calculator/calc/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def calculator(request):
+    result = ''
+    if request.method == 'POST':
+        expression = request.POST.get('expression','')
+        try:
+            result = eval(expression)
+        except Exception as e:
+            result = "Error" 
+    return render (request,'calculator.html',{'result':result})
