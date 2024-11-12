2e10528f8e614b4ab8ed66747f409f4cd756a4e9
abbas <bmnswry27@gmail.com>
2024-11-12 22:45:45 +0330

    viws ,settings,urls

--- calculator/calc/views.py
+++ calculator/calc/views.py
@@ -11,4 +11,4 @@ def calculator(request):
             result = eval(expression)
         except Exception as e:
             result = "Error" 
-    return render (request,'calculator.html',{'result':result})
+    return render (request,'index.html',{'result':result})
